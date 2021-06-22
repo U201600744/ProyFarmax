8c489d3065e115d382747f2337f348242b8c9a45
u201603325 <u201603325@upc.edu.pe>
2021-06-22 01:12:28 -0500

    estructura de carpetas

--- app/src/main/java/com/proyecto/proyfarmax/MainActivity.java
+++ app/src/main/java/com/proyecto/proyfarmax/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.proyecto.proyfarmax.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
